fa0f26325e370c71e3b2762fddebe853381d1eff
yuebinyun <yuebinyun@gmail.com>
2018-08-08 18:09:32 +0800

    update

--- 003-janus-api-test.py
+++ 003-janus-api-test.py
@@ -1,35 +1,23 @@
 #!/usr/bin/env python3
 
 import requests
-import code
-import sys
 import time
 import json
 import string
 import random
 import six
-from six.moves.urllib.parse import urlparse
 import websocket
+
 
 def t_generator(size=12, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-# def get_encoding():
-#     encoding = getattr(sys.stdin, "encoding", "")
-#     if not encoding:
-#         return "utf-8"
-#     else:
-#         return encoding.lower()
-
 
 OPCODE_DATA = (websocket.ABNF.OPCODE_TEXT, websocket.ABNF.OPCODE_BINARY)
-# ENCODING = get_encoding()
-
 
 
 def main():
-    ws = websocket.create_connection("ws://10.0.17.102:8188", subprotocols=['janus-protocol'])
-    print("Press Ctrl+C to quit...")
+    ws = websocket.create_connection("ws://127.0.0.1:8188", subprotocols=['janus-protocol'])
 
     def recv():
         try:
@@ -57,78 +45,82 @@ def main():
         # console.write(data)
         return data
 
-    msg = {"janus":"info", "transaction": t_generator()}
+    msg = {"janus": "info", "transaction": t_generator()}
     ws.send(json.dumps(msg))
-    print("info: ",  json.loads(recv_ws())["version_string"])
+    print("info: ", json.loads(recv_ws())["version_string"])
     time.sleep(1)
 
-    msg = {"janus":"create", "transaction": t_generator()}
+    msg = {"janus": "create", "transaction": t_generator()}
     ws.send(json.dumps(msg))
     session_id = json.loads(recv_ws())["data"]["id"]
-    print("session id: " , session_id)
+    print("session id: ", session_id)
     time.sleep(1)
 
-    msg = {"janus":"keepalive", "transaction": t_generator(), "session_id": session_id}
+    msg = {"janus": "keepalive", "transaction": t_generator(), "session_id": session_id}
     ws.send(json.dumps(msg))
-    print("keepalive ack: ",  json.loads(recv_ws()))
+    print("keepalive ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
-    msg = {"janus":"attach", "transaction": t_generator(),  "plugin": "janus.plugin.videoroom", "session_id": session_id}
+    msg = {"janus": "attach", "transaction": t_generator(), "plugin": "janus.plugin.videoroom",
+           "session_id": session_id}
     ws.send(json.dumps(msg))
     handle_id = json.loads(recv_ws())["data"]["id"]
-    print("handle_id: " , handle_id)
+    print("handle_id: ", handle_id)
     time.sleep(1)
 
     room = random.randint(100000, 999999)
 
     body = {"room": room, "permanent": False, "description": "melot room", "request": "create"}
-    msg = {"janus":"message", "transaction": t_generator(),  "handle_id": handle_id, "session_id": session_id, "body": body}
+    msg = {"janus": "message", "transaction": t_generator(), "handle_id": handle_id, "session_id": session_id,
+           "body": body}
     ws.send(json.dumps(msg))
-    print("creat room ack: ",  json.loads(recv_ws()))
+    print("creat room ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
-    body =  { "request": "join", "room": room, "ptype": "publisher", "display": "ban" }
-    msg = {"janus":"message", "transaction": t_generator(),  "handle_id": handle_id, "session_id": session_id, "body": body}
+    body = {"request": "join", "room": room, "ptype": "publisher", "display": "ban"}
+    msg = {"janus": "message", "transaction": t_generator(), "handle_id": handle_id, "session_id": session_id,
+           "body": body}
     ws.send(json.dumps(msg))
-    print("join room ack: ",  json.loads(recv_ws()))
+    print("join room ack: ", json.loads(recv_ws()))
     time.sleep(1)
-    print("join room ack: ",  json.loads(recv_ws()))
-    time.sleep(1)
-
-    print("查询参与者...")
-    r = requests.get('http://localhost:8080/api/v1/participants?page=1&per_page=5')
-    print(r.text)
+    print("join room ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
-    body =  { "request": "leave"}
-    msg = {"janus":"message", "transaction": t_generator(),  "handle_id": handle_id, "session_id": session_id, "body": body}
+    # print("查询参与者...")
+    # r = requests.get('http://localhost:8080/api/v1/participants?page=1&per_page=5')
+    # print(r.text)
+    # time.sleep(1)
+
+    body = {"request": "leave"}
+    msg = {"janus": "message", "transaction": t_generator(), "handle_id": handle_id, "session_id": session_id,
+           "body": body}
     ws.send(json.dumps(msg))
-    print("leave room ack: ",  json.loads(recv_ws()))
+    print("leave room ack: ", json.loads(recv_ws()))
     time.sleep(1)
-    print("leave room ack: ",  json.loads(recv_ws()))
+    print("leave room ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
+    # print("查询参与者...")
+    # r = requests.get('http://localhost:8080/api/v1/participants?page=1&per_page=5')
+    # print(r.text)
+    # time.sleep(1)
+    # print()
 
-    print("查询参与者...")
-    r = requests.get('http://localhost:8080/api/v1/participants?page=1&per_page=5')
-    print(r.text)
-    time.sleep(1)
-    print()
-
-    body = {"room": room, "request": "destroy" }
-    msg = {"janus":"message", "transaction": t_generator(),  "handle_id": handle_id, "session_id": session_id, "body": body}
+    body = {"room": room, "request": "destroy"}
+    msg = {"janus": "message", "transaction": t_generator(), "handle_id": handle_id, "session_id": session_id,
+           "body": body}
     ws.send(json.dumps(msg))
-    print("destroy room ack: ",  json.loads(recv_ws()))
+    print("destroy room ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
-    msg = {"janus":"detach", "transaction": t_generator(),  "handle_id": handle_id, "session_id": session_id}
+    msg = {"janus": "detach", "transaction": t_generator(), "handle_id": handle_id, "session_id": session_id}
     ws.send(json.dumps(msg))
-    print("detach ack: ",  json.loads(recv_ws()))
+    print("detach ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
-    msg = {"janus":"destroy", "transaction": t_generator(), "session_id": session_id}
+    msg = {"janus": "destroy", "transaction": t_generator(), "session_id": session_id}
     ws.send(json.dumps(msg))
-    print("destroy ack: ",  json.loads(recv_ws()))
+    print("destroy ack: ", json.loads(recv_ws()))
     time.sleep(1)
 
     return
